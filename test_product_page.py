593b7e01d0685d3e3142be29ad749430642f4984
dmitry030595 <dmitry030595@gmail.com>
2023-09-07 12:47:34 +0600

    add xfail test

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,16 @@
+import pytest
+
 from QL_project.pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo', ["?promo=offer0", "?promo=offer1",
+                                   "?promo=offer2", "?promo=offer3",
+                                   "?promo=offer4", "?promo=offer5",
+                                   "?promo=offer6", pytest.param(
+        "?promo=offer7", marks=pytest.mark.xfail),
+                                   "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, promo):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at" \
+           f"-work_207/{promo}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_product_page()
