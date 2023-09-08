36065091f870890eca17a1a22fbe68c1dacb63d4
dmitry030595 <dmitry030595@gmail.com>
2023-09-08 15:33:24 +0600

    add comment

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ def test_guest_can_add_product_to_basket(browser, promo):
     product_page.open()
     product_page.should_be_product_page()
 
-
+# второй способ
 # product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 # promo = [f"{product_base_link}?promo=offer{no}" for no in range(10)]
 
